83417117763a89046672133edda35460185e7c83
Mahesh19 <30853110+MaheshMahi19@users.noreply.github.com>
2019-01-28 23:59:37 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,78 @@
+import json
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+x = {}
+client = commands.Bot(command_prefix="'")
+@client.event
+async def on_ready():
+    print("I am ready for CTF's")
+
+@client.command(pass_context=True)
+async def addctf(ctx,ctfname:str,date:int,month:str,url:str,*,desc):
+    server = ctx.message.server
+    if ctfname in x:
+        await client.say("CTF is already in List\n If you want to edit use 'edit command")
+        return
+    x[ctfname] = {"url":url,"date":date,"month":month,"desc":desc,"challs":{}}
+    await client.say("CTF has been Added")
+    print(x)
+@client.command(pass_context=True)
+async def editctf(ctx,ctfname:str,date:int,month:int,url:str,*,ndesc:str):
+    if ctfname in x:
+        x[ctfname] = {"url":url,"date":date,"month":month,"desc":ndesc}
+        await client.say("CTF has been Updated")
+        print(x)
+    else:
+        await client.say("Not found any Related CTF")
+@client.command(pass_context=True)
+async def ctf(ctx,count:int):
+    for i in range(count):
+        y = x.keys()
+        y = list(y)
+        xc = x[y[i]]
+        em = discord.Embed(title=f"A short info About {y[i]}",colour=discord.Colour.red())
+        em.add_field(name="Date and month of CTF challenge",value="%s/%s/2019"%(xc["date"],xc["month"]))
+        em.add_field(name="Link for the CTF",value=f"[{y[i].upper()}](https://{xc['url']})")
+        em.add_field(name="Description",value="%s"%(xc["desc"]))
+        await client.say(embed=em)
+
+@client.command(pass_context=True)
+async def addchall(ctx,ctfname,points:int,*,chall:str):
+    if ctfname in x:
+        y = x[ctfname]["chall"]
+        if chall in y:
+            await client.say("Challenge is Already inserted")
+            return
+        y[chall] = {"points":points}
+        await client.say(f"Challenge has been added to {ctfname}")
+        print(x)
+    else:
+        await client.say("Nothing is there to add Challenge")
+@client.command(pass_context=True)
+async def challs(ctx,ctfname):
+    if ctfname not in x:
+        await client.say("No CTF named to show Challenges")
+        return
+    challs = x[ctfname]["challs"]
+    em = discord.Embed(title="Challenge Name",description="Points of Challenge")
+    for i,j in challs.items():
+        em.add_field(name=i,value=j["points"])
+    await client.say(embed=em)
+client.remove_command("help")
+@client.command(pass_context=True)
+async def help(ctx):
+    em = discord.Embed()
+    em.add_field(name="Adding a CTF challenge",value="'addctf <ctfname> <date> <month> <link> <description>",inline=False)
+    em.add_field(name="Edit a CTF challenge",value="'editctf <ctfname> <date> <month> <link> <description>",inline=False)
+    em.add_field(name="To check CTF's",value="'ctf <number>",inline=False)
+    em.add_field(name="Add challenge to a CTF",value="'addchall <ctfname> <points> <challenge Name>",inline=False)
+    em.add_field(name="To check Challenges in a CTF",value="'challs <ctfname>",inline=False)
+    await client.say(embed=em)
+@client.command(pass_context=True)
+async def total(ctx):
+    await client.say(f"Its the Present list **{x}**")
+    
+client.run("NTM5NDc2MzI4NjIyMzI1ODEy.DzC6Cg.e4h5RCju908JpSU7Hf9JLXpQYH8")
+print(x)    
